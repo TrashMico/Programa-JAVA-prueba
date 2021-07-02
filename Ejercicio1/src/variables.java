2564bba9f2bf1f7d8834d78a7188a95f7c302e37
TrashMico <03matiascarrasco@gmail.com>
2021-07-01 21:26:35 -0400

    l

---
+++ Ejercicio1/src/variables.java
@@ -0,0 +1,15 @@
+package src;
+
+public class variables {
+    
+   public static void varimain(String[] args) {
+       byte edad = 50;
+       byte edad2= 60;
+
+       if (edad<edad2){
+            System.out.println("xdddddddd");
+       }
+        
+    }
+
+    }
